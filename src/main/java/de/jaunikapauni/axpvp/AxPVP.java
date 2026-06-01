7e0506187ae85f88eb68423061d913b5c62cdbdb
JauniKapauni <riches-goods.54@icloud.com>
2026-06-01 19:35:54 +0200

    init: initialize project

---
+++ src/main/java/de/jaunikapauni/axpvp/AxPVP.java
@@ -0,0 +1,16 @@
+package de.jaunikapauni.axpvp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AxPVP extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
