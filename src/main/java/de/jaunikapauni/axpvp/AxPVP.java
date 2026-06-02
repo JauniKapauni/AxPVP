4ffb4b61b3ca6afd6ce44395de0f474dd0a27336
JauniKapauni <riches-goods.54@icloud.com>
2026-06-02 17:19:42 +0200

    feat: add DatabaseManager

--- src/main/java/de/jaunikapauni/axpvp/AxPVP.java
+++ src/main/java/de/jaunikapauni/axpvp/AxPVP.java
@@ -1,12 +1,24 @@
 package de.jaunikapauni.axpvp;
 
+import de.jaunikapauni.axpvp.manager.DatabaseManager;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AxPVP extends JavaPlugin {
+    DatabaseManager databaseManager;
+    public DatabaseManager getDatabaseManager(){
+        return databaseManager;
+    }
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        saveDefaultConfig();
+        databaseManager = new DatabaseManager(this);
+        if (!databaseManager.initDatabaseTable1()) {
+            getLogger().severe("Error creating db table!");
+            Bukkit.getServer().shutdown();
+        }
     }
 
     @Override
